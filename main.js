c74676d8d445104aca3e804c64484a204ccb67a5
Matias Nicotra <matiasanicotra@gmail.com>
2024-05-27 16:09:31 -0300

    algoritmo para verificar ganador

--- main.js
+++ main.js
@@ -33,7 +33,20 @@ function playerPlays() {
       cell.addEventListener("click", (e) => {
         board[row][column] = "O";
         cell.textContent = board[row][column];
-        PCPlays();
+
+        turn = 1;
+        const won = checkIfWinner();
+
+        if (won === "none") {
+          PCPlays();
+          return;
+        }
+        
+        if (won === "draw") {
+          renderDraw();
+          cell.removeEventListener("click", this);
+          return;
+        }
       });
     }
   });
@@ -68,11 +81,25 @@ function PCPlays() {
         }
       }
     }
+
     turn = 0;
     renderBoard();
     renderCurrentPlayer();
-    playerPlays();
-  }, 500);
+    const won = checkIfWinner();
+    
+    if (won === "none") {
+      playerPlays();
+      return;
+    }
+
+    if (won === "draw") {
+      renderDraw();
+    }
+  }, 1500);
+}
+
+function renderDraw() {
+  playerDiv.textContent = "Draw";
 }
 
 function checkIfCanWin() {
@@ -114,13 +141,61 @@ function checkIfCanWin() {
 
   //filtra y devuelve resultados
   const results = [s1, s2, s3, s4, s5, s6, s7, s8].filter((line) => {
-    retrurn (
+    return (
       line[0].value + line[1].value + line[2].value === 2 ||
       line[0].value + line[1].value + line[2].value === -4
     );
   });
 
   return results;
+}
+
+function checkIfWinner() {
+  const p1 = board[0][0];
+  const p2 = board[0][1];
+  const p3 = board[0][2];
+  const p4 = board[1][0];
+  const p5 = board[1][1];
+  const p6 = board[1][2];
+  const p7 = board[2][0];
+  const p8 = board[2][1];
+  const p9 = board[2][2];
+
+  const s1 = [p1, p2, p3];
+  const s2 = [p4, p5, p6];
+  const s3 = [p7, p8, p9];
+  const s4 = [p1, p4, p7];
+  const s5 = [p2, p5, p8];
+  const s6 = [p3, p6, p9];
+  const s7 = [p1, p5, p9];
+  const s8 = [p3, p5, p7];
+
+  const results = [s1, s2, s3, s4, s5, s6, s7, s8].filter((line) => {
+    return (
+      line[0].value + line[1].value + line[2].value === "XXX" ||
+      line[0].value + line[1].value + line[2].value === "OOO"
+    );
+  });
+
+  if (results.length > 0) {     // hay un ganador
+    if (res[0][0] === "X") {
+      playerDiv.textContent = "PC WINS";
+      return "pcwon";
+    } else {
+      playerDiv.textContent = "USER WINS";
+      return "userwon";
+    } 
+  } else {
+    let draw = true;
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        if (board[i][j] === "") {
+          draw = false;
+        }
+      }
+    }
+    return draw ? "draw" : "none";
+  }
 }
 
 function renderCurrentPlayer() {
